91c537b7b5eba730d781f3f0c0de80435ffbdc71
domen111 <domen111@gmail.com>
2019-04-18 17:32:56 +0800

    deploy.sh / zshrc

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cp -i vimrc ~/.vimrc
+cp -i tmux.conf ~/.tmux.conf
+
+# zsh
+wget -O ~/.zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+cp -i zshrc.local ~/.zshrc.local
